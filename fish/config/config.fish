1dd86bf29d884cb2a25a67a193c4a05452b37f64
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2021-03-26 14:24:05 +0900

    add fishの設定をいれた

---
+++ fish/config/config.fish
@@ -0,0 +1,3 @@
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/s4m0r1/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/s4m0r1/Downloads/google-cloud-sdk/path.fish.inc'; end
